bd39f37e511bae5857a5059532d248c7dd618c3d
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2023-07-12 17:30:33 -0600

    Creación DTO que se utilizará para importar el archivo de limites comunes

---
+++ Application/DTOs/LimiteMaximoComunDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs
+{
+    internal class LimiteMaximoComunDto
+    {
+        public string ClaveParametro { get; set; } = string.Empty;
+        public string LDM { get; set; } = string.Empty;
+        public string LPC { get; set; } = string.Empty;
+        public string LimiteConsiderado { get; set; } = string.Empty;
+    }
+}
