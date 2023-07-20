dadd5978be0dc2ed8e263e36929a4b778d3ffc93
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2023-07-20 16:40:29 -0600

    Se agrega formulario y validaciones al modal de sustitución

--- WebApp/src/app/modules/muestreo/sustitucion-limites/maximo-comun/maximo-comun.component.ts
+++ WebApp/src/app/modules/muestreo/sustitucion-limites/maximo-comun/maximo-comun.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BaseService } from 'src/app/shared/services/base.service';
 
 @Component({
@@ -11,14 +12,42 @@ export class MaximoComunComponent extends BaseService implements OnInit {
     super();
   }
 
+  form = new FormGroup({
+    origenLimites: new FormControl('', Validators.required),    
+    periodo: new FormControl('', Validators.required),
+    excelLimites: new FormControl(),
+    archivo: new FormControl()    
+  }); 
+
   tipoSeleccionado: string = '';
   periodoSeleccionado: string = '';
   archivoLimites: File = {} as File;
+  archivoRequerido: boolean = false;
 
   ngOnInit(): void {}
   
-  mostrarValoresSeleccionados(){
-    console.log(this.archivoLimites.name);
-    
+  onSubmit(){
+    console.table(this.form.value); 
+  }
+
+  validarArchivo(){    
+    if (this.form.controls.origenLimites.value == 'tablaTemporal') {
+      this.form.controls.excelLimites.setValue('');
+      this.form.controls.excelLimites.setValidators(Validators.required);
+    } else {
+      this.form.controls.excelLimites.setValue('');
+      this.form.controls.excelLimites.setValidators(null);
+    }
+
+    this.form.controls.excelLimites.updateValueAndValidity();
+  }
+  
+  onFileChange(event: any){
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.form.patchValue({
+        archivo: file
+      });
+    }
   }
 }
