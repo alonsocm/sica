64119b09df71aefb273ee70391f6546dc66ac9d5
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2023-08-07 13:16:28 -0600

    Backend - Se agrega repositorio de la tabla HistorialSustitucionLimites

---
+++ Persistence/Repository/HistorialSustitucionLimitesRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces.IRepositories;
+using Domain.Entities;
+using Persistence.Contexts;
+
+namespace Persistence.Repository
+{
+    public class HistorialSustitucionLimitesRepository : Repository<HistorialSustitucionLimites>, IHistorialSusticionLimiteRepository
+    {
+        public HistorialSustitucionLimitesRepository(SicaContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
