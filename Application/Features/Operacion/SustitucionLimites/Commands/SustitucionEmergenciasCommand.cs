2fd4ec165407c41589ec40e842b36b18509a38c2
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2023-08-17 16:22:05 -0600

    Backend - Se implementa la validación de los límites de todos los parámetros.

--- Application/Features/Operacion/SustitucionLimites/Commands/SustitucionEmergenciasCommand.cs
+++ Application/Features/Operacion/SustitucionLimites/Commands/SustitucionEmergenciasCommand.cs
@@ -7,12 +7,12 @@ using Persistence.Repository;
 
 namespace Application.Features.Operacion.SustitucionLimites.Commands
 {
-    public class SustitucionEmergenciasCommand : IRequest<Response<bool>>
+    public class SustitucionEmergenciasCommand : IRequest<Response<List<string>>>
     {
         public ParametrosSustitucionLimitesDto ParametrosSustitucion { get; set; }
     }
 
-    public class SustitucionEmergenciasCommandHandler : IRequestHandler<SustitucionEmergenciasCommand, Response<bool>>
+    public class SustitucionEmergenciasCommandHandler : IRequestHandler<SustitucionEmergenciasCommand, Response<List<string>>>
     {
         private readonly IMuestreoEmergenciasRepository _muestreoEmergenciaRepository;
         private readonly ILimiteParametroLaboratorioRepository _limiteParametroLaboratorioRepository;
@@ -31,13 +31,13 @@ namespace Application.Features.Operacion.SustitucionLimites.Commands
             _historialSustitucionLimiteRepository=historialSusticionLimiteRepository;
         }
 
-        public async Task<Response<bool>> Handle(SustitucionEmergenciasCommand request, CancellationToken cancellationToken)
+        public async Task<Response<List<string>>> Handle(SustitucionEmergenciasCommand request, CancellationToken cancellationToken)
         {
             var resultadosSustituir = await _muestreoEmergenciaRepository.ObtenerResultadosParaSustitucion();
 
             if (!resultadosSustituir.Any())
             {
-                return new Response<bool>(false, "No se encontraron resultados para el período seleccionado.");
+                return new Response<List<string>>() { Succeded = false, Message = "No se encontraron resultados para el período seleccionado." };
             }
 
             var parametrosFiltrados = resultadosSustituir.Select(s => s.IdParametro).Distinct();
@@ -48,6 +48,13 @@ namespace Application.Features.Operacion.SustitucionLimites.Commands
             if (request.ParametrosSustitucion.OrigenLimites == (int)TipoSustitucionLimites.CatalogoLimites)
             {
                 var limitesParametros = await _vwLimiteMaximoComunRepository.ObtenerElementosPorCriterioAsync(x => parametrosFiltrados.Contains(x.ParametroId));
+
+                var parametrosSinLimites = resultadosSustituir.Select(s => new { s.IdParametro, s.ClaveParametro }).Distinct().Where(r => !limitesParametros.Select(s => s.ParametroId).Contains(r.IdParametro));
+
+                if (parametrosSinLimites.Any())
+                {
+                    return new Response<List<string>>() { Data = parametrosSinLimites.Select(s => s.ClaveParametro).ToList(), Succeded=false, Message = "No se encontraron los límites de algunos parámetros" };
+                }
 
                 foreach (var resultado in resultadosSustituir.ToList())
                 {
@@ -74,6 +81,12 @@ namespace Application.Features.Operacion.SustitucionLimites.Commands
             else if (request.ParametrosSustitucion.OrigenLimites == (int)TipoSustitucionLimites.TablaTemporal)
             {
                 var tablaTemporalLimites = request.ParametrosSustitucion.LimitesComunes;
+                var parametrosSinLimites = resultadosSustituir.Select(s => s.ClaveParametro).Distinct().Where(r => !request.ParametrosSustitucion.LimitesComunes.Select(s => s.ClaveParametro).Contains(r));
+
+                if (parametrosSinLimites.Any())
+                {
+                    return new Response<List<string>>() { Data = parametrosSinLimites.ToList(), Succeded=false, Message = "La tabla temporal no contiene los límites de los parámetros indicados" };
+                }
 
                 foreach (var resultado in resultadosSustituir.ToList())
                 {
@@ -116,7 +129,7 @@ namespace Application.Features.Operacion.SustitucionLimites.Commands
                 }); ;
             }
 
-            return new Response<bool>(true);
+            return new Response<List<string>>() { Succeded=true };
         }
     }
 }
