e95df1a31c6a0771e7866e8dd9d76b7a2c699aac
Nayeli Rojas <nayeli230587@gmail.com>
2023-08-22 12:47:09 -0600

    se agregan mensajes de sutitucion en enums

--- WebApp/src/app/shared/enums/tipoMensaje.ts
+++ WebApp/src/app/shared/enums/tipoMensaje.ts
@@ -3,3 +3,8 @@ export enum TipoMensaje {
   Alerta = "warning",
   Error = "danger",
 }
+
+export enum MensajesSustitución {
+  NoExistenResultados = "No existen resultados a sustituir para los años seleccionados",
+  PrimeraVez = "Es la primera vez que se realizará la sustitución, da clic en 'Sustituir limites'"
+}
