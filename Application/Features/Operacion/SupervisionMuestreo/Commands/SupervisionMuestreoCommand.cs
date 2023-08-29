b6e51ee2c0e27578b8e1f96ffd742affadd29fe5
Nayeli Rojas <nayeli230587@gmail.com>
2023-08-29 17:57:53 -0600

    Se agregan repositorios para vista de organismosDirecciones, se realiza api para la obtencion de organismosDirecciones, ajuste en api de guardado de supervision de muestreo.

--- Application/Features/Operacion/SupervisionMuestreo/Commands/SupervisionMuestreoCommand.cs
+++ Application/Features/Operacion/SupervisionMuestreo/Commands/SupervisionMuestreoCommand.cs
@@ -3,6 +3,7 @@ using Application.Enums;
 using Application.Features.Operacion.SustitucionLimites.Commands;
 using Application.Interfaces.IRepositories;
 using Application.Wrappers;
+using AutoMapper;
 using Domain.Entities;
 using MediatR;
 using System;
@@ -18,31 +19,29 @@ namespace Application.Features.Operacion.SupervisionMuestreo.Commands
         public SupervisionMuestreoDto supervision { get; set; }
     }
 
-    public class SupervisionMuestreoCommandHandler : IRequestHandler<SupervisionMuestreoCommand, Response<bool>>
+    public class SupervisionMuestreoCommandHandler :  IRequestHandler<SupervisionMuestreoCommand, Response<bool>>
     {
-       
-        private readonly ISupervisionMuestreoRepository _supervisionMuestreoRepository;
+        private readonly IRepositoryAsync<Domain.Entities.SupervisionMuestreo> _repositoryAsync;
         
-        public SupervisionMuestreoCommandHandler(ISupervisionMuestreoRepository supervisionRepository)
+        private IMapper _mapper;
+     
+
+        public SupervisionMuestreoCommandHandler(IRepositoryAsync<Domain.Entities.SupervisionMuestreo> repositoryAsync, IMapper mapper )
         {
-            _supervisionMuestreoRepository = supervisionRepository;
+            _repositoryAsync = repositoryAsync;           
+            _mapper = mapper;
       
         }
 
         public async Task<Response<bool>> Handle(SupervisionMuestreoCommand request, CancellationToken cancellationToken)
         {
-           
+         
             if (request.supervision.Id != 0) { }
             else
-            {
-           
-
+            {                
+               var newSupervision = _mapper.Map<Domain.Entities.SupervisionMuestreo>(request);
+               var data =  await _repositoryAsync.AddAsync(newSupervision);
             }
-
-
-
-
-
             return new Response<bool>(true);
 
         }
