5ec3ca974b3ae703603535018b085bf9138f0edb
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2023-09-06 13:09:15 -0600

    Backend - Cambio en DTO.

--- Application/DTOs/SupervisionMuestreoDto.cs
+++ Application/DTOs/SupervisionMuestreoDto.cs
@@ -15,10 +15,10 @@
         public string SupervisorConagua { get; set; }
         public long SitioId { get; set; }
         public string ClaveSitio { get; set; }
-        public string NombreSitio { get; set; }
-        public string LatitudSitio { get; set; }
-        public string LongitudSitio { get; set; }
-        public string TipoCuerpoAgua { get; set; }
+        public string? NombreSitio { get; set; }
+        public string? LatitudSitio { get; set; }
+        public string? LongitudSitio { get; set; }
+        public string? TipoCuerpoAgua { get; set; }
         public string ClaveMuestreo { get; set; }
         public float LatitudToma { get; set; }
         public float LongitudToma { get; set; }
