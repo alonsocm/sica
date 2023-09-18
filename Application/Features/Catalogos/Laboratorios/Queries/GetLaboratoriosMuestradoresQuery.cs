e168e2ead5f986de5926034f007d90b0bb4a92c3
Nayeli Rojas <nayeli230587@gmail.com>
2023-09-18 17:23:41 -0600

    Se crea query y api para los laboratorios que solamente tienenn muestradores

---
+++ Application/Features/Catalogos/Laboratorios/Queries/GetLaboratoriosMuestradoresQuery.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces.IRepositories;
+using Application.Wrappers;
+using MediatR;
+
+namespace Application.Features.Catalogos.Laboratorios.Queries
+{
+    public class GetLaboratoriosMuestradoresQuery : IRequest<Response<List<Domain.Entities.Laboratorios>>>
+    {
+    }
+
+    public class GetLaboratoriosMuestradoresHandler : IRequestHandler<GetLaboratoriosMuestradoresQuery, Response<List<Domain.Entities.Laboratorios>>>
+    {
+        private readonly ILaboratorioRepository _repositoryAsync;
+
+
+        public GetLaboratoriosMuestradoresHandler(ILaboratorioRepository repositoryAsync)
+        {
+            this._repositoryAsync = repositoryAsync;
+
+        }
+        public async Task<Response<List<Domain.Entities.Laboratorios>>> Handle(GetLaboratoriosMuestradoresQuery request, CancellationToken cancellationToken)
+        {
+            return new Response<List<Domain.Entities.Laboratorios>>(_repositoryAsync.ObtenerLaboratoriosMuestradores());
+        }
+    }
+}
--- Application/Interfaces/IRepositories/ILaboratorioRepository.cs
+++ Application/Interfaces/IRepositories/ILaboratorioRepository.cs
@@ -1,13 +1,9 @@
 ﻿using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Interfaces.IRepositories
 {
     public interface ILaboratorioRepository : IRepository<Laboratorios>
     {
+        List<Laboratorios> ObtenerLaboratoriosMuestradores();
     }
 }
