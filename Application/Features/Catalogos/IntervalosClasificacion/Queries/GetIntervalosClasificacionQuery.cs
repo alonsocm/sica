7d4decc6745a825c9994cbf313889292b4543730
Nayeli Rojas <nayeli230587@gmail.com>
2023-09-18 12:11:36 -0600

    Se crea api para obtener catalogo de intervalos

---
+++ Application/Features/Catalogos/IntervalosClasificacion/Queries/GetIntervalosClasificacionQuery.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces.IRepositories;
+using Application.Wrappers;
+using MediatR;
+
+namespace Application.Features.Catalogos.IntervalosClasificacion.Queries
+{
+    public class GetIntervalosClasificacionQuery : IRequest<Response<List<Domain.Entities.IntervalosClasificacion>>>
+    {
+    }
+
+    public class GetIntervalosClasificacionHandler : IRequestHandler<GetIntervalosClasificacionQuery, Response<List<Domain.Entities.IntervalosClasificacion>>>
+    {
+        private readonly IRepositoryAsync<Domain.Entities.IntervalosClasificacion> _repositoryAsync;
+
+        public GetIntervalosClasificacionHandler(IRepositoryAsync<Domain.Entities.IntervalosClasificacion> repositoryAsync)
+        {
+            this._repositoryAsync = repositoryAsync;
+
+        }
+        public async Task<Response<List<Domain.Entities.IntervalosClasificacion>>> Handle(GetIntervalosClasificacionQuery request, CancellationToken cancellationToken)
+        {
+            var datos = await _repositoryAsync.ListAsync();
+            return new Response<List<Domain.Entities.IntervalosClasificacion>>(datos);
+
+        }
+    }
+
+
+}
---
+++ WebAPI/Controllers/v1/Catalogos/IntervalosClasificacion.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Catalogos.IntervalosClasificacion.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.v1.Catalogos
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    public class IntervalosClasificacion : BaseApiController
+    {
+        [HttpGet("IntervalosClasificacion")]
+        public async Task<IActionResult> GetIntervalosClasificacionQuery()
+        {
+            return Ok(await Mediator.Send(new GetIntervalosClasificacionQuery()));
+        }
+    }
+}
