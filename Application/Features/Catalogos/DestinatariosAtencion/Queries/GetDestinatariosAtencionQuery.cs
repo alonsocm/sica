86f6f40053d74d69e7b2910dedda76bd0ee53b21
Nayeli Rojas <nayeli230587@gmail.com>
2023-09-25 17:13:27 -0600

    Consulata api y query para consulta de catalogo Mes y DestinatariosAtencion

---
+++ Application/Features/Catalogos/DestinatariosAtencion/Queries/GetDestinatariosAtencionQuery.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces.IRepositories;
+using Application.Wrappers;
+using MediatR;
+
+namespace Application.Features.Catalogos.DestinatariosAtencion.Queries
+{
+    public class GetDestinatariosAtencionQuery : IRequest<Response<List<Domain.Entities.DestinatariosAtencion>>>
+    {
+    }
+    public class GetDestinatariosAtencionHandler : IRequestHandler<GetDestinatariosAtencionQuery, Response<List<Domain.Entities.DestinatariosAtencion>>>
+    {
+        private readonly IRepositoryAsync<Domain.Entities.DestinatariosAtencion> _repositoryAsync;
+
+        public GetDestinatariosAtencionHandler(IRepositoryAsync<Domain.Entities.DestinatariosAtencion> repositoryAsync)
+        {
+            _repositoryAsync = repositoryAsync;
+        }
+        public async Task<Response<List<Domain.Entities.DestinatariosAtencion>>> Handle(GetDestinatariosAtencionQuery request, CancellationToken cancellationToken)
+        {
+            return new Response<List<Domain.Entities.DestinatariosAtencion>>(await _repositoryAsync.ListAsync());
+        }
+    }
+}
