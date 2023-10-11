82b18406cbb4abd7b7e9bc415903a08d57b66253
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2023-10-11 14:04:56 -0600

    Backend - Funcionalidad para obtener los metadatos de una imagen.

---
+++ Application/DTOs/EvidenciasMuestreo/ImageInformationDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.DTOs.EvidenciasMuestreo
+{
+    public class ImageInformationDto
+    {
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public string? Aperture { get; set; }
+        public string? FocalLength { get; set; }
+        public string? Iso { get; set; }
+        public string? Flash { get; set; }
+        public string? Shutter { get; set; }
+        public DateTime? DateTime { get; set; }
+        public string? Height { get; set; }
+        public string? Width { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public double? Altitude { get; set; }
+        public double? Direction { get; set; }
+    }
+}
---
+++ Shared/Utilities/Services/MetadataExtractorService.cs
@@ -0,0 +1,49 @@
+﻿using Application.DTOs.EvidenciasMuestreo;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.Jpeg;
+
+namespace Shared.Utilities.Services
+{
+    public class MetadataExtractorService
+    {
+        public static ImageInformationDto GetMetadaFromImage(Stream stream)
+        {
+            var directories = ImageMetadataReader.ReadMetadata(stream);
+            ImageInformationDto imageInformation = new();
+
+            var gpsDirectory = directories.OfType<GpsDirectory>().FirstOrDefault();
+            var exifDirectory = directories.OfType<ExifDirectoryBase>().FirstOrDefault();
+            var jpegDirectory = directories.OfType<JpegDirectory>().FirstOrDefault();
+
+            if (gpsDirectory != null)
+            {
+                var geolocalization = gpsDirectory.GetGeoLocation();
+                imageInformation.Latitude = geolocalization?.Latitude;
+                imageInformation.Longitude = geolocalization?.Longitude;
+                imageInformation.Altitude = gpsDirectory.GetDouble(GpsDirectory.TagAltitude);
+                imageInformation.Direction = gpsDirectory.GetDouble(GpsDirectory.TagImgDirection);
+            }
+
+            if (exifDirectory != null)
+            {
+                imageInformation.Make = exifDirectory.GetDescription(ExifDirectoryBase.TagMake);
+                imageInformation.Model = exifDirectory.GetDescription(ExifDirectoryBase.TagModel);
+                imageInformation.Aperture = exifDirectory.GetDescription(ExifDirectoryBase.TagAperture);
+                imageInformation.FocalLength = exifDirectory.GetDescription(ExifDirectoryBase.TagFocalLength);
+                imageInformation.Iso = exifDirectory.GetDescription(ExifDirectoryBase.TagIsoSpeed);
+                imageInformation.Flash = exifDirectory.GetDescription(ExifDirectoryBase.TagFlash);
+                imageInformation.Shutter = exifDirectory.GetDescription(ExifDirectoryBase.TagShutterSpeed);
+                imageInformation.DateTime = exifDirectory.GetDescription(ExifDirectoryBase.TagDateTime) != null ? exifDirectory.GetDateTime(ExifDirectoryBase.TagDateTime) : null;
+            }
+
+            if (jpegDirectory != null)
+            {
+                imageInformation.Height = jpegDirectory.GetDescription(JpegDirectory.TagImageHeight);
+                imageInformation.Width = jpegDirectory.GetDescription(JpegDirectory.TagImageWidth);
+            }
+
+            return imageInformation;
+        }
+    }
+}
