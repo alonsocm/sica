1f503d9a4b0c5cbe264c729efd9714f1cd4bc20e
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2024-06-27 13:52:19 -0600

    Se modifica el método de paginación. Si no se envía número de página, regresa todos los registros.

--- Application/Wrappers/PagedResponse.cs
+++ Application/Wrappers/PagedResponse.cs
@@ -21,25 +21,29 @@
 
         public static PagedResponse<List<T>> CreatePagedReponse(List<T> data, int page, int pageSize)
         {
-            page = page < 1 ? 1 : page;
-            pageSize = pageSize > 30 ? 10 : pageSize;
-            var totalRecords = data.Count;
-            var totalPages = (double)totalRecords / pageSize;
-            int roundedTotalPages = Convert.ToInt32(Math.Ceiling(totalPages));
             var items = data;
 
-            if (totalRecords > pageSize)
+            if (page >= 1)
             {
-                items = data.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                var totalRecords = data.Count;
+                var totalPages = (double)totalRecords / pageSize;
+                int roundedTotalPages = Convert.ToInt32(Math.Ceiling(totalPages));
+
+                if (totalRecords > pageSize)
+                {
+                    items = data.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                }
+
+                return new PagedResponse<List<T>>(items, page, pageSize)
+                {
+                    TotalPages = roundedTotalPages,
+                    TotalRecords = totalRecords
+                };
             }
-
-            var response = new PagedResponse<List<T>>(items, page, pageSize)
+            else
             {
-                TotalPages = roundedTotalPages,
-                TotalRecords = totalRecords
-            };
-
-            return response;
+                return new PagedResponse<List<T>>(items, page, pageSize);
+            }
         }
     }
 }
