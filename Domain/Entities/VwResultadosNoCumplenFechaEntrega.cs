c0f93a2f0daba57b79b1e228457b04b5b73a3a3d
Nayeli Rojas <Nayeli Rojas@CE0012L1102>
2024-07-02 10:16:42 -0600

    Se remapea, creando vista para la obtencion de las claves parametros con su respectivo muestreo de los resultados que no cumplieron con la validacion de reglas

---
+++ Domain/Entities/VwResultadosNoCumplenFechaEntrega.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities;
+
+public partial class VwResultadosNoCumplenFechaEntrega
+{
+    public long MuestreoId { get; set; }
+
+    public string ClaveMuestreo { get; set; } = null!;
+
+    public DateTime FechaEntrega { get; set; }
+
+    public DateTime? FechaMaxima { get; set; }
+
+    public DateTime? FechaRealVisita { get; set; }
+
+    public string ClaveParametro { get; set; } = null!;
+}
