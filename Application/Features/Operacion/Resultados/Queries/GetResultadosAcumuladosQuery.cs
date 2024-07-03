78a64facfc5b27ca93cbb431b8090fb771a75ce2
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2024-07-03 14:56:59 -0600

    Se agrega funcionalidad para filtrado en la pantalla "Resumen de la validación de reglas por resultado"

--- Application/Features/Operacion/Resultados/Queries/GetResultadosAcumuladosQuery.cs
+++ Application/Features/Operacion/Resultados/Queries/GetResultadosAcumuladosQuery.cs
@@ -27,11 +27,10 @@ namespace Application.Features.Operacion.Resultados.Queries
         public async Task<PagedResponse<IEnumerable<AcumuladosResultadoDto>>> Handle(GetResultadosMuestreoEstatusMuestreoPaginadosQuery request, CancellationToken cancellationToken)
         {
             var data = await _repositoryAsync.GetResultadosMuestreoEstatusMuestreoAsync(request.estatusId);
-            data = data.AsQueryable();
 
             if (request.Filter.Any())
             {
-                var expressions = MuestreoExpression.GetExpressionList(request.Filter);
+                var expressions = QueryExpression<AcumuladosResultadoDto>.GetExpressionList(request.Filter);
                 List<AcumuladosResultadoDto> lstMuestreo = new();
 
                 foreach (var filter in expressions)
@@ -39,27 +38,26 @@ namespace Application.Features.Operacion.Resultados.Queries
                     if (request.Filter.Count == 2 && request.Filter[0].Conditional == "equals" && request.Filter[1].Conditional == "equals")
                     {
                         var dataFinal = data;
-                        dataFinal = (IEnumerable<AcumuladosResultadoDto>)dataFinal.AsQueryable().Where(filter);
+                        dataFinal = dataFinal.AsQueryable().Where(filter);
                         lstMuestreo.AddRange(dataFinal);
+                        data = lstMuestreo;
                     }
                     else
                     {
-                        data = (IEnumerable<AcumuladosResultadoDto>)data.AsQueryable().Where(filter);
+                        data = data.AsQueryable().Where(filter);
                     }
                 }
-
-                data = (lstMuestreo.Count > 0) ? lstMuestreo.AsQueryable() : data;
             }
 
             if (request.OrderBy != null)
             {
                 if (request.OrderBy.Type == "asc")
                 {
-                    data = (IEnumerable<AcumuladosResultadoDto>)data.AsQueryable().OrderBy(MuestreoExpression.GetOrderByExpression(request.OrderBy.Column));
+                    data = data.AsQueryable().OrderBy(QueryExpression<AcumuladosResultadoDto>.GetOrderByExpression(request.OrderBy.Column));
                 }
                 else if (request.OrderBy.Type == "desc")
                 {
-                    data = (IEnumerable<AcumuladosResultadoDto>)data.AsQueryable().OrderByDescending(MuestreoExpression.GetOrderByExpression(request.OrderBy.Column));
+                    data = data.AsQueryable().OrderByDescending(QueryExpression<AcumuladosResultadoDto>.GetOrderByExpression(request.OrderBy.Column));
                 }
             }
 
