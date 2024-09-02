f71246c42c0d59b4d46bb0b4881aed141c7bda9f
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2024-09-02 17:52:11 -0600

    Refactor DeleteResultadosByFilterCommand and Handler
    Cleaned up using directives. Modified DeleteResultadosByFilterCommand to use ResultadosIds instead of Filters and estatusId. Updated DeleteResultadosByFilterCommandHandler constructor to remove IMuestreoRepository dependency. Simplified Handle method to check ResultadosIds and call Eliminar on _resultadoRepository or return failure if empty. Removed logic for filtering and deleting based on Filters and estatusId.

--- Application/Features/Operacion/Resultados/Comands/DeleteResultadosByFilterCommand.cs
+++ Application/Features/Operacion/Resultados/Comands/DeleteResultadosByFilterCommand.cs
@@ -1,50 +1,30 @@
-﻿using Application.Expressions;
-using Application.Interfaces.IRepositories;
-using Application.Interfaces;
+﻿using Application.Interfaces.IRepositories;
 using Application.Wrappers;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain.Entities;
-using System.Reflection.Metadata.Ecma335;
 
 namespace Application.Features.Operacion.Resultados.Comands
 {
     public class DeleteResultadosByFilterCommand : IRequest<Response<bool>>
     {
-        public List<Filter> Filters { get; set; }
-        public int estatusId { get; set; }
-
+        public IEnumerable<long> ResultadosIds { get; set; }
     }
 
     public class DeleteResultadosByFilterCommandHandler : IRequestHandler<DeleteResultadosByFilterCommand, Response<bool>>
     {
-        private readonly IMuestreoRepository _repositoryAsync;
         private readonly IResultado _resultadoRepository;
-        public DeleteResultadosByFilterCommandHandler(IMuestreoRepository repositoryAsync, IResultado resultadoRepository)
+        public DeleteResultadosByFilterCommandHandler(IResultado resultadoRepository)
         {
-            _repositoryAsync = repositoryAsync;
             _resultadoRepository = resultadoRepository;
         }
         public async Task<Response<bool>> Handle(DeleteResultadosByFilterCommand request, CancellationToken cancellationToken)
         {
-            var data = await _repositoryAsync.GetResultadosMuestreoEstatusMuestreoAsync(request.estatusId);
-            data = data.AsQueryable();
-
-            if (request.Filters.Any())
+            if (request.ResultadosIds.Any())
             {
-                var expressions = MuestreoExpression.GetExpressionList(request.Filters);
-                foreach (var filter in expressions)
-                { data = (IEnumerable<DTOs.AcumuladosResultadoDto>)data.AsQueryable().Where(filter); }
+                _resultadoRepository.Eliminar(x => request.ResultadosIds.Contains(x.Id));
+                return new Response<bool> { Succeded = true };
             }
 
-            List<long> lstresultados = data.Select(x => x.ResultadoMuestreoId).Distinct().ToList();
-            foreach (var idResultado in lstresultados)
-            { _resultadoRepository.Eliminar(x => x.Id == idResultado); }
-            return new Response<bool> { Succeded = true };
+            return new Response<bool> { Succeded = false, Message="No se proporcionaron resultados para eliminar" };
         }
     }
 }
