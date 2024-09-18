7a3cbf9068177628574ed352178cece3bb0eec46
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2024-09-18 14:05:14 -0600

    Add EstatusAux class for enum validation and conversion
    Introduced a new internal class `EstatusAux` in the `Application.Enums` namespace. This class includes a static method `GetEstatus` that validates an integer against the `EstatusMuestreo` enum using `Enum.IsDefined`. If the integer is valid, it returns the corresponding enum value; otherwise, it throws an `ArgumentException` with the message "Estatus muestreo no válido".

---
+++ Application/Enums/EstatusAux.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Enums
+{
+    internal class EstatusAux
+    {
+        internal static EstatusMuestreo GetEstatus(int estatus)
+        {
+            bool isValid = Enum.IsDefined(typeof(EstatusMuestreo), estatus);
+
+            if (isValid)
+            {
+                return (EstatusMuestreo)estatus;
+            }
+            else
+            {
+                throw new ArgumentException("Estatus muestreo no válido");
+            }
+        }
+    }
+}
