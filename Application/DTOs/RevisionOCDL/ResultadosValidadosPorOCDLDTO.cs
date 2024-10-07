deed8f0b5116623616800830636c1b03489fd643
carlitosluque <carlitospade381@gmail.com>
2024-10-07 13:35:52 -0600

    Creación de DTO para resultados validados OCDL.

---
+++ Application/DTOs/RevisionOCDL/ResultadosValidadosPorOCDLDTO.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+
+namespace Application.DTOs.RevisionOCDL
+{
+    public class ResultadosValidadosPorOCDLDTO
+    {
+        public string NoEntregaOCDL { get; set; } = string.Empty;
+        public string? ClaveUnica { get; set; }
+        public string ClaveSitio { get; set; } = string.Empty;
+        public string ClaveMonitoreo { get; set; } = string.Empty;
+        public string NombreSitio { get; set; } = string.Empty;
+        public string? ClaveParametro { get; set; }
+        public string Laboratorio { get; set; } = string.Empty;        
+        public string? TipoCuerpoAgua{ get; set; }
+        public string? TipoCuerpoAguaOriginal { get; set; }
+        public string? Resultado { get; set; }
+        public string TipoAprobacion { get; set; } = string.Empty;
+        public string? EsCorrectoResultado { get; set; }
+        public string? Observaciones { get; set; }
+        public string? FechaLimiteRevision { get; set; }
+        public string NombreUsuario { get; set; } = string.Empty;
+        public string? FechaRealizacion { get; set; }
+        public string EstatusResultado { get; set; } = string.Empty;
+        public int EstatusId { get; set; }
+        public int? EstatusOCDL { get; set; }
+
+    }    
+}
