ac99cece23daae5fbf35bd55722b00af9542c061
carlitosluque <carlitospade381@gmail.com>
2024-10-11 13:31:56 -0600

    Añadidos métodos para importar datos desde el backend. Implementación de interfaz

--- WebApp/src/app/modules/muestreo/revision/OCDL/validado/services/resultados-validados.service.ts
+++ WebApp/src/app/modules/muestreo/revision/OCDL/validado/services/resultados-validados.service.ts
@@ -1,9 +1,45 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ResultadosValidadosService {
   constructor(private http: HttpClient) {}
+
+  getResultadosValidadosPorOCDL(
+    page: number,
+    pageSize: number,
+    filter: string,
+    order?: { column: string; type: string }
+  ): Observable<any> {
+    const userId = localStorage.getItem('idUsuario') || '';
+    let params = new HttpParams({
+      fromObject: {
+        page: page,
+        pageSize: pageSize,
+        filter: filter,
+        order: order != null ? order.column + '_' + order.type : '',
+      },
+    });
+    return this.http.get(environment.apiUrl+'/resultados/ResultadosValidadosPorOCDL', { params });
+  }
+
+  getDistinctOCDL(
+    column: string,
+    filter: string,
+  ): Observable<Object> {
+    const params = new HttpParams({
+      fromObject: {
+        column: column,
+        filter: filter,
+      },
+    });
+    return this.http.get(
+      environment.apiUrl + '/resultados/GetDistinctValuesFromColumnOCDL',
+      { params }
+    );
+  }
 }
