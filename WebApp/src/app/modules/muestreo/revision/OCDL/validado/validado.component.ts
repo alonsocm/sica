4f52852bdf95251466d6963f1bf1e65ca2d4921c
carlitosluque <carlitospade381@gmail.com>
2024-10-11 14:27:56 -0600

    Creación de vista donde se muestran los datos de la consulta, implementación de métodos de BaseService

--- WebApp/src/app/modules/muestreo/revision/OCDL/validado/validado.component.ts
+++ WebApp/src/app/modules/muestreo/revision/OCDL/validado/validado.component.ts
@@ -2,18 +2,301 @@ import { Component, OnInit } from '@angular/core';
 import { Column } from 'src/app/interfaces/filter/column';
 import { ICommonMethods } from 'src/app/shared/interfaces/ICommonMethods';
 import { BaseService } from 'src/app/shared/services/base.service';
-
+import { ResultadosValidadosService } from './services/resultados-validados.service';
+import { Resultado } from 'src/app/interfaces/Resultado.interface';
 
 @Component({
   selector: 'app-validado',
   templateUrl: './validado.component.html',
   styleUrls: ['./validado.component.css'],
 })
-
 export class ValidadoComponent
   extends BaseService
   implements OnInit, ICommonMethods
 {
+  resultados: Array<Resultado> = [];
+  resultadosSeleccionados: Array<Resultado> = [];
+  constructor(private resultadosService: ResultadosValidadosService) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.definirColumnas();
+    this.consultarResultados();
+  }
+
+  definirColumnas() {
+    let nombresColumnas: Array<Column> = [
+      {
+        name: 'noEntregaOCDL',
+        label: 'N° ENTREGA OC/DL',
+        order: 1,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'claveUnica',
+        label: 'CLAVE ÚNICA',
+        order: 2,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'claveSitio',
+        label: 'CLAVE SITIO',
+        order: 3,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'claveMonitoreo',
+        label: 'CLAVE MONITOREO',
+        order: 4,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'nombreSitio',
+        label: 'NOMBRE SITIO',
+        order: 5,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'claveParametro',
+        label: 'CLAVE PARÁMETRO',
+        order: 6,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'laboratorio',
+        label: 'LABORATORIO',
+        order: 7,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'tipoCuerpoAgua',
+        label: 'TIPO CUERPO AGUA',
+        order: 8,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'tipoCuerpoAguaOriginal',
+        label: 'TIPO CUERPO AGUA ORIGINAL',
+        order: 9,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'resultado',
+        label: 'RESULTADO',
+        order: 10,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'tipoAprobacion',
+        label: 'TIPO APROBACIÓN',
+        order: 11,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'esCorrectoResultado',
+        label: 'RESULTADO CORRECTO',
+        order: 12,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'observaciones',
+        label: 'OBSERVACIÓN OC/DL',
+        order: 13,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'fechaLimiteRevision',
+        label: 'FECHA LIMITE DE REVISIÓN',
+        order: 14,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'nombreUsuario',
+        label: 'USUARIO QUE REVISÓ',
+        order: 15,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'fechaRealizacion',
+        label: 'FECHA REALIZACIÓN',
+        order: 16,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+      {
+        name: 'estatusResultado',
+        label: 'ESTATUS DEL RESULTADO',
+        order: 17,
+        selectAll: true,
+        filtered: false,
+        asc: false,
+        desc: false,
+        data: [],
+        filteredData: [],
+        dataType: '',
+        selectedData: '',
+      },
+    ];
+    this.columns = nombresColumnas;
+    this.setHeadersList(this.columns);
+  }
+
+  consultarResultados(
+    page: number = this.page,
+    pageSize: number = this.NoPage,
+    filter: string = this.cadena
+  ): void {
+    this.loading = true;
+    this.resultadosService
+      .getResultadosValidadosPorOCDL(page, pageSize, filter, this.orderBy)
+      .subscribe({
+        next: (response: any) => {
+          this.selectedPage = false;
+          this.resultados = response.data;
+          this.page = response.totalRecords !== this.totalItems ? 1 : this.page;
+          this.totalItems = response.totalRecords;
+          this.getPreviousSelected(
+            this.resultados,
+            this.resultadosSeleccionados
+          );
+          this.selectedPage = this.anyUnselected(this.resultados)
+            ? false
+            : true;
+          this.loading = false;
+        },
+        error: (error) => {
+          this.loading = false;
+        },
+      });
+  }
+
+  getPreviousSelected(
+    resultados: Array<Resultado>,
+    resultadosSeleccionados: Array<Resultado>
+  ) {
+    resultados.forEach((f) => {
+      let resultadoSeleccionado = resultadosSeleccionados.find(
+        (x) => f.claveUnica === x.claveUnica
+      );
+
+      if (resultadoSeleccionado != undefined) {
+        f.isChecked = true;
+      }
+    });
+  }
+
   sort(column: string, order: 'asc' | 'desc'): void {
     throw new Error('Method not implemented.');
   }
@@ -32,10 +315,7 @@ export class ValidadoComponent
   onPageClick(page: any): void {
     throw new Error('Method not implemented.');
   }
-  getPreviousSelected(registros: Array<any>, registrosSeleccionados: Array<any>): void {
-    throw new Error('Method not implemented.');
-  }
-  ngOnInit(): void {
+  onExportarResultadosClick(): void {
     throw new Error('Method not implemented.');
   }
 }
