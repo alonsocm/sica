bef8e7946571f458fb5351a20b5ab2a20c127939
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2024-10-15 14:44:54 -0600

    Add GetResultados query and handler with new endpoint
    Significantly modified GetResultados.cs:
    - Changed GetResultados class to public and implemented IRequest<PagedResponse<List<ResultadoLiberacionDTO>>>
    - Added properties: Filters, PageSize, and Page
    - Introduced GetResultadosHandler class to handle requests using IResultado repository
    
    Updated Resultados.cs:
    - Added using directive for Application.Features.Operacion.LiberacionResultados.Queries
    - Added new HttpGet endpoint in WebAPI.Controllers.v1.Operacion to fetch results based on filters, page size, and page number via Mediator

--- Application/Features/Operacion/LiberacionResultados/Queries/GetResultados.cs
+++ Application/Features/Operacion/LiberacionResultados/Queries/GetResultados.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.DTOs.LiberacionResultados;
+using Application.Interfaces.IRepositories;
+using Application.Wrappers;
+using MediatR;
 
 namespace Application.Features.Operacion.LiberacionResultados.Queries
 {
-    internal class GetResultados
+    public class GetResultados : IRequest<PagedResponse<List<ResultadoLiberacionDTO>>>
     {
+        public List<Filter> Filters { get; set; }
+        public int PageSize { get; set; }
+        public int Page { get; set; }
+    }
+
+    public class GetResultadosHandler : IRequestHandler<GetResultados, PagedResponse<List<ResultadoLiberacionDTO>>>
+    {
+        private readonly IResultado _resultadoRepository;
+
+        public GetResultadosHandler(IResultado resultadoRepository)
+        {
+            _resultadoRepository = resultadoRepository;
+        }
+
+        public async Task<PagedResponse<List<ResultadoLiberacionDTO>>> Handle(GetResultados request, CancellationToken cancellationToken)
+        {
+            var data = await _resultadoRepository.GetResultadosLiberacion(request.Filters, request.Page, request.PageSize);
+
+            return data;
+        }
     }
 }
