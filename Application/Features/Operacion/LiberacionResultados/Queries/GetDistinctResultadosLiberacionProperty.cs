3b77e57593f2c0a286a5b2c25a6f06740bf76214
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2024-10-15 20:29:55 -0600

    Add query and handler for distinct "Resultados Liberacion"
    Introduced a new query `GetDistinctResultadosLiberacionProperty` and its handler to fetch distinct properties of "Resultados Liberacion". The query class encapsulates request parameters, while the handler processes the request by interacting with the `IResultado` repository asynchronously.

---
+++ Application/Features/Operacion/LiberacionResultados/Queries/GetDistinctResultadosLiberacionProperty.cs
@@ -0,0 +1,25 @@
+﻿using Application.DTOs.Common;
+using Application.Interfaces.IRepositories;
+using MediatR;
+
+namespace Application.Features.Operacion.LiberacionResultados.Queries
+{
+    public class GetDistinctResultadosLiberacionProperty : QueryHelperDTO, IRequest<IEnumerable<object>>
+    {
+    }
+
+    public class GetDistinctResultadosLiberacionPropertyHandler : IRequestHandler<GetDistinctResultadosLiberacionProperty, IEnumerable<object>>
+    {
+        private readonly IResultado _resultadoRepository;
+
+        public GetDistinctResultadosLiberacionPropertyHandler(IResultado resultadoRepository)
+        {
+            _resultadoRepository = resultadoRepository;
+        }
+
+        public async Task<IEnumerable<object>> Handle(GetDistinctResultadosLiberacionProperty request, CancellationToken cancellationToken)
+        {
+            return await _resultadoRepository.GetDistinctResultadosLiberacionPropertyAsync(request.Filters, request.Selector);
+        }
+    }
+}
