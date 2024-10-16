822842bbe47f628926bdbbfe7fa2990051bfd6c5
alonsocm <ALONSO.CASMAX@GMAIL.COM>
2024-10-15 20:29:13 -0600

    Add QueryHelperDTO class for query operations
    Introduced a new namespace `Application.DTOs.Common` and added the `QueryHelperDTO` class within it. This class includes properties for filtering (`Filters`), pagination (`PageNumber`, `PageSize`), and selection (`Selector`). The `Filters` property is initialized to an empty list, and `Selector` is initialized to an empty string.

---
+++ Application/DTOs/Common/QueryHelperDTO.cs
@@ -0,0 +1,12 @@
+﻿using Application.Wrappers;
+
+namespace Application.DTOs.Common
+{
+    public class QueryHelperDTO
+    {
+        public List<Filter> Filters { get; set; } = new List<Filter>();
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string Selector { get; set; } = string.Empty;
+    }
+}
